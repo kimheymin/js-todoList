2a9ba87afaeb1dee068b086cf782d709254f0b04
김혜민 <keg9317@thirautech.com>
2023-02-24 13:02:30 +0900

    클릭 시 상태 변경

--- todo.js
+++ todo.js
@@ -1,41 +1,58 @@
 const form = document.querySelector(".todoForm");
 const input = document.querySelector(".todoInput");
-const div = document.querySelector('.todoList');
-const ul = document.querySelector('.todos');
+const div = document.querySelector(".todoList");
+const ul = document.querySelector(".todos");
 
-const darkmodeButton = document.querySelector('.darkmodeButton');
+const darkmodeButton = document.querySelector(".darkmodeButton");
+let active = true;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const li = document.createElement("li");
-  li.setAttribute('class','item');
+  li.setAttribute("class", "item");
+
+  const value = input.value;
+  if (value.trim().length === 0) {
+    return;
+  }
+
   li.innerText = input.value;
 
-  const button = document.createElement('button');
-  button.setAttribute('class','delBtn');
-  button.innerText = 'del';
+  const button = document.createElement("button");
+  button.setAttribute("class", "delBtn");
+  button.innerText = "del";
 
-  const divider = document.createElement('div');
-  divider.setAttribute('class','divider');
+  const divider = document.createElement("div");
+  divider.setAttribute("class", "divider");
 
   li.appendChild(button);
   ul.appendChild(li);
   ul.appendChild(divider);
   div.appendChild(ul);
 
-  input.value = '';
+  input.value = "";
   input.focus();
 
-  button.addEventListener('click',()=>{
+  button.addEventListener("click", () => {
     ul.removeChild(li);
     ul.removeChild(divider);
   });
+
+  li.addEventListener("click", (e) => {
+    active = !active;
+
+    if (active) {
+      li.style.textDecoration = "line-through";
+      li.style.color = "gray";
+    } else {
+      li.style.textDecoration = null;
+      li.style.color = null;
+    }
+  });
 });
 
-darkmodeButton.addEventListener('click',()=>{
-  const body = document.querySelector('body');
-  body.style.backgroundColor = 'black';
-})
-
-
+darkmodeButton.addEventListener("click", () => {
+  const body = document.querySelector("body");
+  body.style.backgroundColor = "black";
+});
